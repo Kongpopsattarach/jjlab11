930e9f46262210744fb22937c9440cea41f2047a
JJ <kongpopsattarach.edu@gmail.com>
2023-01-03 17:26:34 +0700

    update

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,26 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    double i = 0;
+    double s = 0;
+    double e = 0;
+    ifstream  source;
+    string line;
+    source.open("score.txt");
+    
+    while (getline(source,line))
+    {
+        i++;
+        s += stod(line);
+        e += pow(stod(line),2);
+    }
+    cout << "Number of data = " << i << "\n";
+    cout << setprecision(3) ;
+    cout << "Mean = " << s/i << "\n";
+    cout << "Standard deviation = " << sqrt(e/i-pow(s/i,2));
+}
